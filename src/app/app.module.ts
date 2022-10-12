78d0bbccafc52466aecacab33c662bbf0626f1c4
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2022-10-12 13:48:09 -0300

    Fixing appComponent import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 //Modules
 import { PagesModule } from './pages/pages.module';
 
 //Components
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
